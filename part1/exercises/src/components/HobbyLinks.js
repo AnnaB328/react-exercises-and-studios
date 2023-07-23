748e6863f761188dfb5616c1be03b57774090989
AnnaB328 <annabsmith328@gmail.com>
2023-07-23 16:44:03 -0500

    Final

--- part1/exercises/src/components/HobbyLinks.js
+++ part1/exercises/src/components/HobbyLinks.js
@@ -1,15 +1,15 @@
 function HobbyLinks() {
-    HobbyLinks = [
+    let hobbyLinks = [
         "https://theimprovshop.com/",
         "https://the1870studio.com/shop",
         "https://rollercon.com/"
     ]
     return (
         <div>
-           <h3>{HobbyLinks}</h3>
-           <a href = {HobbyLinks[0]}>Improv Comedy</a>
-           <a href = {HobbyLinks[1]}>Fun Crafts and Art</a>
-           <a href = {HobbyLinks[2]}>Roller Derby</a>
+           <h3>{hobbyLinks}</h3>
+           <a href = {hobbyLinks[0]}>Improv Comedy</a>
+           <a href = {hobbyLinks[1]}>Fun Crafts and Art</a>
+           <a href = {hobbyLinks[2]}>Roller Derby</a>
         </div>      
     )
 };
