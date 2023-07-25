9f70cd669d9bd88590d8957cda24c9306f31cc14
AnnaB328 <annabsmith328@gmail.com>
2023-07-24 21:31:20 -0500

    Done for now

---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,13 @@
+export default function HobbyIntroduction () {
+    return (
+        <div>
+            <h2>3 Fun Facts About this Hobby</h2>
+            <ol>
+                <li>It allows you to be a kid again by using imagination.</li>
+                <li>I have made some of my best friends through this hobby.</li>
+                <li>Many famous comedians and actors got their start by doing improv.</li>
+            </ol>
+        </div>
+    );
+}
+
