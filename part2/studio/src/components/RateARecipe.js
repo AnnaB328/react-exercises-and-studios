daf97cd389061f3ffc9b445e7536af7fa082c4aa
AnnaB328 <annabsmith328@gmail.com>
2023-07-24 21:06:30 -0500

    Back to basics

--- part2/studio/src/components/RateARecipe.js
+++ part2/studio/src/components/RateARecipe.js
@@ -1,11 +1,8 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe(props) {
-  function GiveRating() {
-    return <h3>{stars[props.rating - 1]}</h3>;
-  }
-  return props.rating <= 5 && props.rating >= 1 ? <GiveRating /> :null;
-}
+function RateARecipe() {
+return stars;  
+};
 
 export default RateARecipe;
 
